5ce994a860d9e43192e0db1838cf92a538a93d5e
AnimeshKatigar <katigaranimesh@gmail.com>
2024-02-24 11:45:57 +0530

    Added classname to hide scrollbar in main body

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,27 +3,25 @@ import "./globals.css";
 import Navbar from "@/components/Navbar.jsx";
 import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export const metadata = {
   title: "MAMIBO",
   description: "A Jewellery Website",
-  keywords: ['Fashion', 'Outfits', 'customized dresses', "Dress", "Gen Z"],
+  keywords: ["Fashion", "Outfits", "customized dresses", "Dress", "Gen Z"],
 };
 
 export const viewport = {
-  width: 'device-width',
+  width: "device-width",
   initialScale: 1,
   maximumScale: 1,
   userScalable: false,
   // Also supported by less commonly used
   // interactiveWidget: 'resizes-visual',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className="no-scrollbar">
         <Navbar />
         {children}
         <Footer />
